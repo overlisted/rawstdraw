aa6e60386cc07394fb7b8514acfa333f238b6425
Ilya Maximov <mail@overlisted.net>
2020-11-21 13:04:28 +0100

    add support for different target OSes

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 extern crate cc;
+extern crate target_lexicon;
+
+use target_lexicon::OperatingSystem;
+use std::str::FromStr;
 
 fn main() {
     cc::Build::new()
@@ -6,7 +10,13 @@ fn main() {
         .warnings(false)
         .compile("librawdraw.a");
 
-    let libs = vec!["X11", "m", "pthread", "Xinerama", "Xext", "GL"];
+    let target_env = std::env::var("TARGET").unwrap();
+    let triple = target_lexicon::Triple::from_str(&target_env).unwrap();
+
+    let libs = match triple.operating_system {
+        OperatingSystem::Linux => vec!["X11", "m", "pthread", "Xinerama", "Xext", "GL"],
+        _ => panic!("Unsupported target operating system!")
+    };
         
     for name in libs {
         println!("cargo:rustc-link-lib={}", name)        
